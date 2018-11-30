e427b87bb1485d0fe75ab61b6ef515a353364fe2
kideg20 <45255539+kideg20@users.noreply.github.com>
2018-11-30 12:26:15 +0200

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -2,7 +2,7 @@
 #additional packeges
 #wget 
 #yum install wget
-#wget 
+#wget https://raw.githubusercontent.com/kideg20/new-server/master/auto.sh
 sudo yum update -y 
 sudo rpm -ivh https://repo.zabbix.com/zabbix/3.4/rhel/7/x86_64/zabbix-release-3.4-2.el7.noarch.rpm
 sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
